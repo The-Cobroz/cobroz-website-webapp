92d0fcdb7a5cdc7241d96bb297b60e9fcbbf96f3
Khushaal Sharma <khushaalsharma1@gmail.com>
2024-03-29 01:46:31 +0530

    New Commit - Web Application Navbar full responsive

--- cobroz-client/src/Components/Webapp/AppNavbar.js
+++ cobroz-client/src/Components/Webapp/AppNavbar.js
@@ -1,65 +1,44 @@
 import React from 'react';
 import "bootstrap/dist/css/bootstrap.css";
-import cobrozLogo from "../../Images/cobroz-icon.png"
+import cobrozLogo from "../../Images/cobroz-icon.png";
 import "./AppStyles.css";
 
 const AppNavbar = () => {
     return (
-        <nav className="navbar navbar-expand-md bg-body-tertiary">
-            <div className="container-fluid">
-                <div className='navbar-brand cobroz-logo' onClick={() => {window.location.href = "/app"}}>
-                    <img src={cobrozLogo} alt='cobroz-logo'/>
+        <div className='cobroz-webapp-navbar'>
+            <div className='cobroz-webapp-brand'>
+                <img src={cobrozLogo} alt='Cobroz'  onClick={() => {window.location.href = "/app"}}/>
+            </div>
+            <div className='navbar-webapp-options'>
+                <div className='navbar-option-icon' onClick={() => {window.location.href = "/app/post/new"}}>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-square-plus">
+                        <rect width="18" height="18" x="3" y="3" rx="2" />
+                        <path d="M8 12h8" />
+                        <path d="M12 8v8" />
+                    </svg>
                 </div>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse websiteLinks" id="navbarSupportedContent">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
-                            <a className="nav-link" aria-current="page" href='/app/post'>
-                                <div>
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-circle-plus"><circle cx="12" cy="12" r="10"/><path d="M8 12h8"/><path d="M12 8v8"/></svg>
-                                    <p>POST</p>
-                                </div>
-                            </a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href='/app/search'>
-                                <div>
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-search"><circle cx="11" cy="11" r="8"/><path d="m21 21-4.3-4.3"/></svg>
-                                    <p>SEARCH</p>
-                                </div>
-                            </a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" aria-current="page" href='/app/chat'>
-                                <div>
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-message-circle-more"><path d="M7.9 20A9 9 0 1 0 4 16.1L2 22Z"/><path d="M8 12h.01"/><path d="M12 12h.01"/><path d="M16 12h.01"/></svg>
-                                    <p>CHAT</p>
-                                </div>
-                            </a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" aria-current="page" href='/app/notifs'>
-                                <div>
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-bell"><path d="M6 8a6 6 0 0 1 12 0c0 7 3 9 3 9H3s3-2 3-9"/><path d="M10.3 21a1.94 1.94 0 0 0 3.4 0"/></svg>
-                                    <p>NOTIFICATIONS</p>
-                                </div>
-                            </a>
-                        </li>
-                    </ul>
-                    <div className='navbar-profile-section'>
-                        <a className="nav-link" aria-current="page" href='/app/profile'>
-                            <div>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-circle-user-round"><path d="M18 20a6 6 0 0 0-12 0"/><circle cx="12" cy="10" r="4"/><circle cx="12" cy="12" r="10"/></svg>
-                                <p>YOU</p>
-                            </div>
-                        </a>
-                    </div>
+                <div className='navbar-option-icon' onClick={() => {window.location.href = "/app/notifications"}}>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-bell">
+                        <path d="M6 8a6 6 0 0 1 12 0c0 7 3 9 3 9H3s3-2 3-9" />
+                        <path d="M10.3 21a1.94 1.94 0 0 0 3.4 0" />
+                    </svg>
+                </div>
+                <div className='navbar-option-icon' onClick={() => {window.location.href = "/app/profile"}}>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-circle-user-round">
+                        <path d="M18 20a6 6 0 0 0-12 0" />
+                        <circle cx="12" cy="10" r="4" />
+                        <circle cx="12" cy="12" r="10" />
+                    </svg>
+                </div>
+                <div className='navbar-option-icon' onClick={() => {window.location.href = "/app/settings"}}>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-settings">
+                        <path d="M12.22 2h-.44a2 2 0 0 0-2 2v.18a2 2 0 0 1-1 1.73l-.43.25a2 2 0 0 1-2 0l-.15-.08a2 2 0 0 0-2.73.73l-.22.38a2 2 0 0 0 .73 2.73l.15.1a2 2 0 0 1 1 1.72v.51a2 2 0 0 1-1 1.74l-.15.09a2 2 0 0 0-.73 2.73l.22.38a2 2 0 0 0 2.73.73l.15-.08a2 2 0 0 1 2 0l.43.25a2 2 0 0 1 1 1.73V20a2 2 0 0 0 2 2h.44a2 2 0 0 0 2-2v-.18a2 2 0 0 1 1-1.73l.43-.25a2 2 0 0 1 2 0l.15.08a2 2 0 0 0 2.73-.73l.22-.39a2 2 0 0 0-.73-2.73l-.15-.08a2 2 0 0 1-1-1.74v-.5a2 2 0 0 1 1-1.74l.15-.09a2 2 0 0 0 .73-2.73l-.22-.38a2 2 0 0 0-2.73-.73l-.15.08a2 2 0 0 1-2 0l-.43-.25a2 2 0 0 1-1-1.73V4a2 2 0 0 0-2-2z" />
+                        <circle cx="12" cy="12" r="3" />
+                    </svg>
                 </div>
             </div>
-        </nav>
-    )
-}
+        </div>
+    );
+};
 
-export default AppNavbar
+export default AppNavbar;
